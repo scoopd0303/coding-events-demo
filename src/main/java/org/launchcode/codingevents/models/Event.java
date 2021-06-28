cfd903955536f3bbe53d5ae832c60d27ff98ec53
Scott McDonald <scottmcdonald@Scotts-MacBook-Air.local>
2021-06-28 16:39:54 -0500

    finished chapter-18 reading

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,13 +1,12 @@
 package org.launchcode.codingevents.models;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Chris Bay
@@ -24,6 +23,9 @@ public class Event extends AbstractEntity {
     @NotNull
     private EventDetails eventDetails;
 
+    @ManyToMany
+    private final List<Tag> tags = new ArrayList<>();
+
     @ManyToOne
     @NotNull(message = "Category is required")
     private EventCategory eventCategory;
@@ -35,28 +37,19 @@ public class Event extends AbstractEntity {
 
     public Event() {}
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public EventCategory getEventCategory() { return eventCategory; }
+    public void setEventCategory(EventCategory eventCategory) { this.eventCategory = eventCategory; }
 
-    public EventCategory getEventCategory() {
-        return eventCategory;
-    }
+    public EventDetails getEventDetails() { return eventDetails; }
+    public void setEventDetails(EventDetails eventDetails) { this.eventDetails = eventDetails; }
 
-    public void setEventCategory(EventCategory eventCategory) {
-        this.eventCategory = eventCategory;
-    }
+    public List<Tag> getTags() { return tags; }
 
-    public EventDetails getEventDetails() {
-        return eventDetails;
-    }
-
-    public void setEventDetails(EventDetails eventDetails) {
-        this.eventDetails = eventDetails;
+    public void addTag(Tag tag) {
+        this.tags.add(tag);
     }
 
     @Override
